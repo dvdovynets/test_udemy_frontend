594eb47d089c49a679bda2e16b18fb9376ea69ef
Dmytro Vdovynets <dvdovynets@atlassian.com>
2022-05-09 11:02:55 +0300

    added functions

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Первую часть задания повторить по уроку
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-Проверить, чтобы все работало без ошибок в консоли */
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-let numberOfFilms = +prompt("How many films you have already watched?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("How many films you have already watched?", "");
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+          numberOfFilms = +prompt("How many films you have already watched?", "");
+        }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -33,30 +33,57 @@ const personalMovieDB = {
   privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-  let lastWathedMovie = prompt("What was the last movie you watched?", "");
-  let lastMovieRating = prompt("Please rate that movie?", "");
-  if (lastWathedMovie === ''|| 
-      lastWathedMovie.length > 50 || 
-      lastWathedMovie.length === 0 || 
-      lastWathedMovie == null) {
-    console.log("Error");
-    i--;
-    continue;
-  } else {
-    personalMovieDB.movies[lastWathedMovie] = lastMovieRating;
-    console.log("Done");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let lastWathedMovie = prompt("What was the last movie you watched?", "");
+    let lastMovieRating = prompt("Please rate that movie?", "");
+    if (lastWathedMovie == null || 
+        lastWathedMovie == ''|| 
+        lastWathedMovie.length > 50 || 
+        lastWathedMovie.length === 0) {
+      console.log("Error");
+      i--;
+      continue;
+    } else {
+      personalMovieDB.movies[lastWathedMovie] = lastMovieRating;
+      console.log("Done");
+    }
   }
 }
 
-if (personalMovieDB.count < 10) {
-  console.log("Not so many movies watched");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("You are a regular viewer");
-} else if (personalMovieDB.count > 50) {
-  console.log("You are a cinemaddict");
-} else {
-  console.log("Error");
+rememberMyFilms();
+
+function detectPersonelLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Not so many movies watched");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a regular viewer");
+  } else if (personalMovieDB.count > 50) {
+    console.log("You are a cinemaddict");
+  } else {
+    console.log("Error");
+  }
 }
 
-console.log(personalMovieDB);
+detectPersonelLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const favoriteGenre = prompt(`What is your favorite genre number ${i}?`, "");
+
+    if (favoriteGenre == null || favoriteGenre.length === 0) {
+      i--;
+      continue;
+    } else {
+      personalMovieDB.genres.push(favoriteGenre);
+    }
+  }
+}
